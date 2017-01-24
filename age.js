ffaaada04df90921f4b1eedff3141e46ee36705a
katherine tuter <katherinetuter@gmail.com>
2017-01-23 20:40:09 -0600

    finished javascript practice exercises

---
+++ age.js
@@ -0,0 +1,19 @@
+
+
+var hoursInYear = (24 * 365);
+console.log("Hours in year: ", hoursInYear);
+
+var minutesInDecade = (525600 * 10);
+console.log("Minutes in decade: ", minutesInDecade);
+
+var ageInSeconds = (22 * 31536000);
+console.log("Age in seconds: ", ageInSeconds);
+
+var age = 20;
+
+if (age > 20) {
+	console.log("I'm old")
+}
+else {
+	console.log("I'm young")
+};
