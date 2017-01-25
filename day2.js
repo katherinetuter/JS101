f2a3b7745d7f8368e75a1d1799acbac09e5e7da7
katherine tuter <katherinetuter@gmail.com>
2017-01-24 19:22:10 -0600

    learning switch

---
+++ day2.js
@@ -0,0 +1,22 @@
+//switch statement
+
+var day = "Friday";
+
+switch(day){
+	case "Monday":
+		console.log("Booo");
+		break;
+	case "Friday":
+		console.log("party");
+		break;
+	default: 
+		console.log("not Monday");
+}
+
+if(day === "Monday"){
+	console.log("Booo");
+}else if(day === "Friday"){
+console.log("party");	
+} else{
+console.log("not Monday");
+}
