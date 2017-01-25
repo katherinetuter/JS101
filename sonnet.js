e0c3ef5d4929a5cedc50aa73d76bf96d19775e62
katherine tuter <katherinetuter@gmail.com>
2017-01-24 18:51:44 -0600

    removing consolelogs

--- sonnet.js
+++ sonnet.js
@@ -2,16 +2,14 @@
 // place it in a variable
 
 var element = document.getElementById("sonnet");
-console.log("element", element);
 var sonnet = element.innerHTML;
-console.log(sonnet);
 
 //Find and output the starting position of the 
 //word "orphans"
 
 var indexOfOrphans = sonnet;
 var n = indexOfOrphans.search("orphans");
-console.log("index of orphans: ", n);
+console.log("indexOfOrphans", indexOfOrphans)
 
 //Output the number of characters in the sonnet
 
@@ -21,13 +19,11 @@ console.log("Length of sonnet: ", sonnet.length);
 // "winter" with "yuletide"
 
 var winterSonnet = sonnet.replace("winter", "yuletide");
-console.log(winterSonnet);
 
 //Replace all occurances of the string "the"
 // with "a large"
 
 var aLargeSonnet = winterSonnet.replace(/the /g, "a large ");
-console.log("final sonnet", aLargeSonnet);
 
 //Set the content of the sonnet div 
 //with the new string
